1715db1fa072ec8bb6b385b5a291a3d151a40009
Dan <22314521+danpro94@users.noreply.github.com>
2025-10-15 17:51:45 +0900

    2강 저장 누락후 푸쉬분 추가 커밋

--- 7_selectionSort.py
+++ 7_selectionSort.py
@@ -25,3 +25,48 @@ if __name__ == "__main__":
 
     print("정렬 후 배열:", end=" ")
     print_array(data)
+
+
+## [개선] 이중 선택 정렬 (최댓값도 함께 정렬)
+def double_selection_sort(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        min_index = ■■■■■■
+        max_index = ■■■■■■
+
+        for i in ■■■■■■:
+            if ■■■■■■:
+                min_index = i
+            if ■■■■■■:
+                max_index = i
+
+        if ■■■■■■:
+            arr[left], arr[min_index] = arr[min_index], arr[left]
+
+            if ■■■■■■:
+                max_index = min_index
+
+        if ■■■■■■:
+            arr[right], arr[max_index] = arr[max_index], arr[right]
+
+        left ■■■■■■
+        right ■■■■■■
+
+
+def print_array(arr):
+    print(" ".join(map(str, arr)))
+
+
+if __name__ == "__main__":
+    data = [3, 5, 4, 2, 1]
+
+    print("정렬 전 배열:", end=" ")
+    print_array(data)
+
+    double_selection_sort(data)
+
+    print("정렬 후 배열:", end=" ")
+    print_array(data)
+
